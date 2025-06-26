c9e8004e526be448b26c77fdde06fa0c1a2ac696
Raghavgupta2003 <raghav>
2025-06-27 00:57:34 +0530

    errorfixedduringDeploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,7 +36,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
 //debug
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.send("Socket server is up ✅");
 });
 
